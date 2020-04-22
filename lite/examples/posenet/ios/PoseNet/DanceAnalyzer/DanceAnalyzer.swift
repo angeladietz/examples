09d485071b7f3c617f41e3f8f5cede7690d53f3f
Kara Dietz <kmdietz@edu.uwaterloo.ca>
2020-04-22 17:52:33 -0400

    add first and fifth

--- lite/examples/posenet/ios/PoseNet/DanceAnalyzer/DanceAnalyzer.swift
+++ lite/examples/posenet/ios/PoseNet/DanceAnalyzer/DanceAnalyzer.swift
@@ -20,17 +20,35 @@ class DanceAnalyzer: NSObject {
         
         if person[BodyPart.LEFT_WRIST]!.y > person[BodyPart.LEFT_ELBOW]!.y {
             if person[BodyPart.LEFT_ELBOW]!.y > person[BodyPart.LEFT_SHOULDER]!.y {
-                return "first position"
+                if person[BodyPart.RIGHT_WRIST]!.y > person[BodyPart.RIGHT_ELBOW]!.y {
+                    if person[BodyPart.RIGHT_ELBOW]!.y > person[BodyPart.RIGHT_SHOULDER]!.y {
+                        return "first"
+                    }
+                }
             }
         }
         
-        if person[BodyPart.LEFT_WRIST]!.y < person[BodyPart.LEFT_ELBOW]!.y && person[BodyPart.LEFT_ELBOW]!.y < person[BodyPart.LEFT_SHOULDER]!.y {
+        else if person[BodyPart.LEFT_WRIST]!.y < person[BodyPart.LEFT_ELBOW]!.y && person[BodyPart.LEFT_ELBOW]!.y < person[BodyPart.LEFT_SHOULDER]!.y {
                 if person[BodyPart.RIGHT_WRIST]!.y < person[BodyPart.RIGHT_ELBOW]!.y && person[BodyPart.RIGHT_ELBOW]!.y < person[BodyPart.RIGHT_SHOULDER]!.y {
-                    return "fifth position"
+                    return "fifth"
+            }
+        }
+        //second position
+        //check slope of line
+        else {
+            if (person[BodyPart.LEFT_WRIST]!.x != person[BodyPart.LEFT_SHOULDER]!.x) &&  (person[BodyPart.RIGHT_WRIST]!.x != person[BodyPart.RIGHT_SHOULDER]!.x) {
+                let leftSlope = abs((person[BodyPart.LEFT_WRIST]!.y - person[BodyPart.LEFT_SHOULDER]!.y)/(person[BodyPart.LEFT_WRIST]!.x - person[BodyPart.LEFT_SHOULDER]!.x))
+
+                 let rightSlope = abs((person[BodyPart.RIGHT_WRIST]!.y - person[BodyPart.RIGHT_SHOULDER]!.y)/(person[BodyPart.RIGHT_WRIST]!.x - person[BodyPart.RIGHT_SHOULDER]!.x))
+
+                if leftSlope < 0.5 && rightSlope < 0.5 {
+                    return "second"
+                }
             }
         }
 
+
         
-        return "NOT first position"
+        return "Not Ballet"
     }
 }
